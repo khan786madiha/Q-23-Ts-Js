575d8dd4167bc30c75b6c47bb6b4288046b7c6c2
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-05 10:43:32 -0800

    Q-23 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-23

---
+++ Q23.js
@@ -0,0 +1,40 @@
+"use strict";
+// 23.	Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+Object.defineProperty(exports, "__esModule", { value: true });
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log(car == 'Civic');
+let num = 35;
+console.log("Is num == 35 (in num) ? I predict true otherwise I predict False");
+console.log(num === 35);
+console.log(num === "35");
+let student = "Madiha";
+console.log(student === 'Madiha');
+console.log(student === "Areeba");
+let age = 25;
+console.log("Is age == 25 ?  I predict true otherwise Ipredict false");
+console.log(age === 25);
+console.log(age === 23);
+let a = 3;
+let b = 6;
+if (a > 7 && b > 4) {
+    console.log(" true ");
+}
+else {
+    console.log("false");
+}
+;
+let candidatAge = 23;
+if (candidatAge < 25) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+;
